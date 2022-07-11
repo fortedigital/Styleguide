66fdf0f26283919b62d550bd011372e44ffde8dc
Marek Barszcz <marek.barszcz@fortedigital.no>
2022-07-11 13:59:04 +0200

    Add codegenerator project, serve js files from static

---
+++ Styleguide.JsonGenerator/StyleguideJsonGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace Styleguide.JsonGenerator
+{
+    [Generator]
+    public class StyleguideJsonGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+#if DEBUG
+            if (!Debugger.IsAttached)
+            {
+                //Debugger.Launch();
+            }
+#endif
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+        }
+    }
+}
