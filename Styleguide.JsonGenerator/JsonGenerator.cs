9c3caf05876694207d3a7d02ed96c0287ddc86dc
Marek Barszcz <marek.barszcz@fortedigital.no>
2022-07-27 13:17:25 +0200

    Complete TypeMembersVisitor, clean up

--- Styleguide.JsonGenerator/JsonGenerator.cs
+++ Styleguide.JsonGenerator/JsonGenerator.cs
@@ -26,6 +26,7 @@ namespace Styleguide.JsonGenerator
         private const string StyleguideJsonFileExtensions = ".styleguide.json";
 
         private const string StyleguideDefaultCategoryName = "Default";
+        
         #endregion
         
         private readonly Lazy<INamedTypeSymbol> _systemIEnumerableType;
@@ -34,9 +35,7 @@ namespace Styleguide.JsonGenerator
         private readonly Lazy<INamedTypeSymbol> _styleguideContentTypeAttributeType;
         private readonly Lazy<INamedTypeSymbol> _styleguideItemsContentTypesAttributeType;
         private readonly Lazy<INamedTypeSymbol> _styleguideIgnoreAttributeType;
-
         
-
         public JsonGenerator(CSharpCompilation compilation) : base(compilation)
         {
             _styleguideViewModelForAttributeType = new Lazy<INamedTypeSymbol>(() =>
@@ -72,14 +71,16 @@ namespace Styleguide.JsonGenerator
             .ForAll(source =>
             {
                 var targetDirectory = Directory.GetParent(source.SourceType.GetContainingFilePath()) ?? throw new ArgumentNullException();
-                var targetFileName = source.SourceType.Name.Replace("ViewModel", string.Empty);
+                var targetFileName = source.SourceType.Name.Replace("ViewModel", string.Empty).Replace("Props", String.Empty);
                 Debug.Write($"{targetFileName}: ");
 
-                var properties = source.SourceType.GetMembers().OfType<IPropertySymbol>()
+                var properties = source.SourceType.GetAllMembers().OfType<IPropertySymbol>()
                     .Where(symbol => symbol.DeclaredAccessibility == Accessibility.Public) // get all public
                     .Where(symbol => !symbol.HasAttribute(_styleguideIgnoreAttributeType.Value)); // that are not marked as StyleguideIgnore
 
                 JObject componentContext;
+
+                var propertyVisitor = new TypeMembersVisitor(Compilation);
                 
                 try
                 {
@@ -92,7 +93,7 @@ namespace Styleguide.JsonGenerator
                             new JProperty(
                                 "model", 
                                 new JObject(
-                                    properties.Select(CreateProperty)
+                                    properties.Select(property => property.Accept(propertyVisitor))
                                 )
                             ),
                             new JProperty(
@@ -187,5 +188,135 @@ namespace Styleguide.JsonGenerator
         private string GetCategory(ITypeSymbol model) => (string) model.GetAttributes()
             .FirstOrDefault(attribute => attribute.AttributeClass?.MetadataName == _epiServerContentTypeAttributeType.Value.MetadataName)?
             .NamedArguments.FirstOrDefault(kvp => kvp.Key == "GroupName").Value.Value ?? StyleguideDefaultCategoryName;
+    }
+    
+    public class TypeMembersVisitor : SymbolVisitor<JProperty>
+    {
+        private readonly Lazy<INamedTypeSymbol> iEnumerableType;
+
+        private readonly Lazy<INamedTypeSymbol> styleguideIgnoreType;
+
+        #region ValueType types
+
+        private readonly INamedTypeSymbol doubleType;
+        private readonly INamedTypeSymbol singleType;
+        private readonly INamedTypeSymbol intType;
+        private readonly INamedTypeSymbol guidType;
+        private readonly INamedTypeSymbol dateTimeType;
+        private readonly INamedTypeSymbol booleanType;
+        private readonly INamedTypeSymbol stringType;
+
+        private readonly Dictionary<INamedTypeSymbol, JValue> valueTypesLookup;
+
+        #endregion
+        
+        public TypeMembersVisitor(CSharpCompilation compilation)
+        {
+            iEnumerableType = new Lazy<INamedTypeSymbol>(() =>
+                compilation.GetTypeByMetadataNameOrThrow("System.Collections.IEnumerable"));
+
+            stringType = compilation.GetTypeByMetadataNameOrThrow("System.String");
+
+            #region Initialize Styleguide specific types
+
+            styleguideIgnoreType = new Lazy<INamedTypeSymbol>(() =>
+                compilation.GetTypeByMetadataNameOrThrow(
+                    "Styleguide.JsonGenerator.Annotations.StyleguideIgnoreAttribute"));
+
+            #endregion
+
+            #region Initialize ValueType types
+
+            doubleType = compilation.GetTypeByMetadataNameOrThrow("System.Double");
+            singleType = compilation.GetTypeByMetadataNameOrThrow("System.Single");
+            intType = compilation.GetTypeByMetadataNameOrThrow("System.Int32");
+            guidType = compilation.GetTypeByMetadataNameOrThrow("System.Guid");
+            dateTimeType = compilation.GetTypeByMetadataNameOrThrow("System.DateTime");
+            booleanType = compilation.GetTypeByMetadataNameOrThrow("System.Boolean");
+
+#pragma warning disable RS1024
+            valueTypesLookup = new Dictionary<INamedTypeSymbol, JValue>
+            {
+                { doubleType, new JValue(default(double)) },
+                { singleType, new JValue(default(float)) },
+                { intType, new JValue(default(int)) },
+                { guidType, new JValue(Guid.NewGuid()) },
+                { dateTimeType, new JValue(DateTime.Now) },
+                { booleanType, new JValue(default(bool))}
+            };
+#pragma warning restore RS1024
+
+            #endregion
+
+        }
+        
+        public override JProperty VisitProperty(IPropertySymbol symbol)
+        {
+            if (symbol.Type is IArrayTypeSymbol arrayTypeSymbol)
+            {
+                var elementType = arrayTypeSymbol.ElementType;
+                var arrayElement = elementType.IsFromCodeBase()
+                    ? new JObject(
+                        GetContentTypeNameProperty(elementType),
+                        GetValidMembers(elementType).Select(VisitProperty))
+                    : null;
+
+                return new JProperty(symbol.Name, 
+                    arrayElement != null 
+                    ? new JArray(arrayElement, arrayElement, arrayElement)
+                    : new JArray()
+                );
+            }
+
+            if (symbol.Type is INamedTypeSymbol namedTypeSymbol)
+            {
+                if (namedTypeSymbol.Arity == 1 && namedTypeSymbol.DoesImplement(iEnumerableType.Value))
+                {
+                    var elementType = namedTypeSymbol.TypeArguments.First();
+                    
+                    var arrayElement = elementType.IsFromCodeBase()
+                        ? new JObject(
+                            GetContentTypeNameProperty(elementType),
+                            GetValidMembers(elementType).Select(VisitProperty)
+                        )
+                        : null;
+                    
+                    return new JProperty(symbol.Name, 
+                        arrayElement != null 
+                            ? new JArray(arrayElement, arrayElement, arrayElement)
+                            : new JArray()
+                    );
+                }
+
+                if (namedTypeSymbol.IsValueType)
+                {
+                    return new JProperty(symbol.Name,
+                        valueTypesLookup.TryGetValue(namedTypeSymbol, out var defaultValue)
+                            ? defaultValue
+                            : new JValue(0));
+                }
+            }
+
+            return SymbolEqualityComparer.Default.Equals(symbol.Type, stringType)
+                ? new JProperty(symbol.Name, "")
+                : new JProperty(
+                    symbol.Name,
+                    new JObject(
+                        GetContentTypeNameProperty(symbol.Type),
+                        GetValidMembers(symbol.Type).Select(VisitProperty)
+                    )
+                );
+        }
+        
+        private IEnumerable<IPropertySymbol> GetValidMembers(ITypeSymbol propertyType) => 
+            propertyType
+                .GetAllMembers()
+                .OfType<IPropertySymbol>()
+                .Where(type => type.DeclaredAccessibility == Accessibility.Public)
+                .Where(symbol => !symbol.HasAttribute(styleguideIgnoreType.Value));
+
+        private JProperty GetContentTypeNameProperty(ITypeSymbol propertyType) =>
+            new JProperty("ContentType", propertyType.IsAbstract ? "#PROVIDE_CONCRETE_TYPE#" : propertyType.Name);
+        
     }
 }
--- Styleguide.JsonGenerator/StyleguideJsonGenerator.cs
+++ Styleguide.JsonGenerator/StyleguideJsonGenerator.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Newtonsoft.Json.Linq;
-using Styleguide.JsonGenerator.Extensions;
 
 
 namespace Styleguide.JsonGenerator
@@ -58,119 +55,8 @@ namespace Styleguide.JsonGenerator
         {
             var compilation = (CSharpCompilation)context.Compilation;
 
-            var CourseListProps =
-                compilation.GetTypeByMetadataNameOrThrow(
-                    "KongsbergMaritime.Web.Features.Pages.CoursePortalPage.CourseListProps");
-
-            var properties =
-                CourseListProps.GetAllMembers().OfType<IPropertySymbol>();
-
-            var visitor = new TypeMembersVisitor(compilation);
-            var test = properties.Select(prop => prop.Accept(visitor)).ToList();
-
-            // var generator = new JsonGenerator(compilation);
-            // generator.Run();
-        }
-    }
-    
-    public class TypeMembersVisitor : SymbolVisitor<JProperty>
-    {
-        private readonly Lazy<INamedTypeSymbol> iEnumerableType;
-
-        #region ValueType types
-
-        private readonly INamedTypeSymbol doubleType;
-        private readonly INamedTypeSymbol singleType;
-        private readonly INamedTypeSymbol intType;
-        private readonly INamedTypeSymbol guidType;
-        private readonly INamedTypeSymbol dateTimeType;
-
-        private readonly Dictionary<INamedTypeSymbol, JValue> valueTypesLookup;
-
-        #endregion
-        
-        public TypeMembersVisitor(CSharpCompilation compilation)
-        {
-            iEnumerableType = new Lazy<INamedTypeSymbol>(() =>
-                compilation.GetTypeByMetadataNameOrThrow("System.Collections.IEnumerable"));
-
-            #region Initialize ValueType types
-
-            doubleType = compilation.GetTypeByMetadataNameOrThrow("System.Double");
-            singleType = compilation.GetTypeByMetadataNameOrThrow("System.Single");
-            intType = compilation.GetTypeByMetadataNameOrThrow("System.Int32");
-            guidType = compilation.GetTypeByMetadataNameOrThrow("System.Guid");
-            dateTimeType = compilation.GetTypeByMetadataNameOrThrow("System.DateTime");
-
-#pragma warning disable RS1024
-            valueTypesLookup = new Dictionary<INamedTypeSymbol, JValue>
-            {
-                { doubleType, new JValue(default(double)) },
-                { singleType, new JValue(default(float)) },
-                { intType, new JValue(default(int)) },
-                { guidType, new JValue(Guid.NewGuid()) },
-                { dateTimeType, new JValue(DateTime.Now) },
-            };
-#pragma warning restore RS1024
-
-            #endregion
-
-        }
-        
-        public override JProperty VisitProperty(IPropertySymbol symbol)
-        {
-            if (symbol.Type is IArrayTypeSymbol arrayTypeSymbol)
-            {
-                var arrayElement = arrayTypeSymbol.ElementType.IsFromCodeBase()
-                    ? new JObject(
-                        new JProperty("ContentType", symbol.Type.Name),
-                        arrayTypeSymbol.ElementType.GetAllMembers().OfType<IPropertySymbol>().Select(VisitProperty))
-                    : null;
-
-                return new JProperty(symbol.Name, 
-                    arrayElement != null 
-                    ? new JArray(arrayElement, arrayElement, arrayElement)
-                    : new JArray()
-                );
-            }
-
-            if (symbol.Type is INamedTypeSymbol namedTypeSymbol)
-            {
-                if (namedTypeSymbol.Arity == 1 && namedTypeSymbol.DoesImplement(iEnumerableType.Value))
-                {
-                    var elementType = namedTypeSymbol.TypeArguments.First();
-                    
-                    var arrayElement = elementType.IsFromCodeBase()
-                        ? new JObject(
-                            new JProperty("ContentType", symbol.Type.Name),
-                            elementType.GetAllMembers().OfType<IPropertySymbol>().Select(VisitProperty)
-                        )
-                        : null;
-                    
-                    return new JProperty(symbol.Name, 
-                        arrayElement != null 
-                            ? new JArray(arrayElement, arrayElement, arrayElement)
-                            : new JArray()
-                    );
-                }
-
-                if (namedTypeSymbol.IsValueType)
-                {
-                    return new JProperty(namedTypeSymbol.Name,
-                        valueTypesLookup.TryGetValue(namedTypeSymbol, out var defaultValue)
-                            ? defaultValue
-                            : new JValue(0));
-                }
-            }
-            
-            return new JProperty(
-                symbol.Name, symbol.Type.IsFromCodeBase() 
-                ? new JObject(
-                    new JProperty("ContentType", symbol.Type.Name),
-                    symbol.Type.GetAllMembers().OfType<IPropertySymbol>().Select(VisitProperty)
-                    ) 
-                : new JObject());
-            
+            var generator = new JsonGenerator(compilation);
+            generator.Run();
         }
     }
 }
